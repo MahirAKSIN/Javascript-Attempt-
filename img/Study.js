4825881075368f42e02b0e660330bb28a75daaff
Mahir Aksin <98876295+MahirAKSIN@users.noreply.github.com>
2022-07-02 13:59:39 +0300

    TryJavascripts

---
+++ img/Study.js
@@ -0,0 +1,15 @@
+function welcome(){
+   alert("Hosgeldiniz");
+}
+function byby(){
+    alert("GuleGule");
+}
+function call(){
+    alert("Cagirdin geldim");
+}
+function off(){
+    document.getElementById("result").src="img/off.jpg";
+}
+function openn(){
+    document.getElementById("result").src="img/light.jpg";
+}
